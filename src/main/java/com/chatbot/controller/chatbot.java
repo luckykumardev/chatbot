77e87c5dcbb14543138a04568d1f51ecc61d2b24
luckykumardev <luckyu0020kumar@gmail.com>
2021-01-17 18:47:15 +0530

    multiple questions

--- src/main/java/com/chatbot/controller/chatbot.java
+++ src/main/java/com/chatbot/controller/chatbot.java
@@ -3,8 +3,10 @@ package com.chatbot.controller;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class chatbot {
@@ -21,8 +23,9 @@ public class chatbot {
 	
 	@PostMapping("/help")
 	@ResponseBody
-	public String help2() {
-		return "<div>chalgya</div>";
+	public String help2(@ModelAttribute("choice") String choice) {
+		System.out.println(choice);		
+		return "<div>"+choice+"</div>";
 	}
 	
 }
